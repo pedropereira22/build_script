bdf3bb8473bf3811ae3f1c243ce2669fcc28c323
Pedro Pereira <pedropereirex@gmail.com>
2025-01-22 21:36:46 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,37 +4,31 @@
 rm -rf .repo/local_manifests/
 
 # remove device tree
-rm -rf device/xiaomi/mojito
-rm -rf device/xiaomi/sm6150-common
-rm -rf kernel/xiaomi/mojito
-rm -rf vendor/xiaomi/sm6150-common
+rm -rf device/xiaomi/ziyi
+rm -rf device/xiaomi/sm8450-common
+rm -rf kernel/xiaomi/sm8450
+rm -rf kernel/xiaomi/sm8450-devicetrees
+rm -rf kernel/xiaomi/sm8450-modules
+rm -rf vendor/xiaomi/ziyi
+rm -rf vendor/xiaomi/zsm8450-common
 rm -rf hardware/xiaomi
 
 # Initialize ROM manifest
 repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
 
+# cloning device local_manifests 
+git clone https://github.com/pedropereira22/local_manifests .repo/local_manifests
+
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
-
-# cloning device tree
-git clone https://github.com/Sepidermn/android_device_xiaomi_mojito.git --depth 1 -b test device/xiaomi/mojito
-git clone https://github.com/Sepidermn/android_device_xiaomi_sm6150-common.git --depth 1 -b test device/xiaomi/sm6150-common
-
-# cloning kernel tree
-git clone https://github.com/Sachinpawar86/kernel_xiaomi_mojito.git --depth 1 -b test kernel/xiaomi/mojito
-
-# cloning vendor tree
-git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito.git --depth 1 -b 15 vendor/xiaomi/mojito
-git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 15 vendor/xiaomi/sm6150-common
-
-# cloning hardware tree
-git clone https://github.com/Sepidermn/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
 # Set up th build environment
 . build/envsetup.sh
 
 # Choose the target device
-riseup mojito userdebug
+riseup ziyi userdebug
 
 # Build the ROM (use mka bacon for a full build)
-rise b
+# signed build
+gk -f
+rise sb
